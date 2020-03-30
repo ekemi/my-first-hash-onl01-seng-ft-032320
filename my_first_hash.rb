6db819f97d516825f0719c76b7b3a878ec0d3cf7
edmundo papi <edmundopierre@yahoo.com>
2020-03-30 20:29:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,8 +11,9 @@ def shipping_manifest
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
   the_manifest ={"whale bone corsets"=>5,
   "porcelain vases"=>2,
-"oil paintings"=>3}
-return the_manifest
+  "oil paintings"=>3}
+
+  return the_manifest
 end
 
 def retrieval
@@ -23,7 +24,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-return shipping_manifest["oil paintings"]
+   return shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -33,7 +34,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
- shipping_manifest["muskets"] = 2
+  shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
